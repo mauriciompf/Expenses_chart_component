9c0c02f3ebd40729fed21f1fe319dd530279bbc4
Maurício Farias <53566332+mauriciompf@users.noreply.github.com>
2023-09-22 10:39:25 -0400

    Update BalanceLogo.jsx

---
+++ src/components/balance/BalanceLogo.jsx
@@ -0,0 +1,9 @@
+import logo from "C:/mauricio/front-end_mentor_projects/expenses-chart-component-main/src/assets/images/logo.svg";
+
+export default function BalanceLogo() {
+  return (
+    <div>
+      <img src={logo} alt="Logo" />
+    </div>
+  );
+}
