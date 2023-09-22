5072f6f2b0cb9fb94a0b7d71a600a6309ac18584
Maurício Farias <53566332+mauriciompf@users.noreply.github.com>
2023-09-22 10:40:19 -0400

    Add DataValueTotal.jsx

---
+++ src/components/data/values/DataValueTotal.jsx
@@ -0,0 +1,8 @@
+export default function DataValueTotal() {
+  return (
+    <p>
+      <span className="block">Total this month</span>
+      <span className="block">478.33</span>
+    </p>
+  );
+}
