2ab32b03ee60b2d00b5ab7c1c7e88cb93c8365f0
Maurício Farias <53566332+mauriciompf@users.noreply.github.com>
2023-09-22 10:40:09 -0400

    Add DataValues.jsx

---
+++ src/components/data/values/DataValues.jsx
@@ -0,0 +1,11 @@
+import DataValueTotal from "./DataValueTotal";
+import DataValueLastMouth from "./DataValueLastMouth";
+
+export default function DataValues() {
+  return (
+    <div>
+      <DataValueTotal />
+      <DataValueLastMouth />
+    </div>
+  );
+}
