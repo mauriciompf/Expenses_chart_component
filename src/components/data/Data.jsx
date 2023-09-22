9d0a1d36d925b5ab49cde06f904cb42957f75112
Maurício Farias <53566332+mauriciompf@users.noreply.github.com>
2023-09-22 10:39:44 -0400

    Update Data.jsx

---
+++ src/components/data/Data.jsx
@@ -0,0 +1,11 @@
+import DataView from "./view/DataView";
+import DataValues from "./values/DataValues";
+
+export default function Data() {
+  return (
+    <section>
+      <DataView />
+      <DataValues />
+    </section>
+  );
+}
