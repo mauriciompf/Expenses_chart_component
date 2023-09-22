4ca2c551fa90664331187ffed189e31aa7ab4ca3
Maurício Farias <53566332+mauriciompf@users.noreply.github.com>
2023-09-22 10:40:32 -0400

    Add DataView.jsx

---
+++ src/components/data/view/DataView.jsx
@@ -0,0 +1,10 @@
+import DataViewGraph from "./DataViewGraph";
+
+export default function DataView() {
+  return (
+    <div>
+      <p>Spending - Last 7 days</p>
+      <DataViewGraph />
+    </div>
+  );
+}
