8b23784d946b06c09b720ae0cd2aeac40582e59a
Maurício Farias <53566332+mauriciompf@users.noreply.github.com>
2023-09-25 19:23:36 -0400

    Update Data.jsx

--- src/components/data/Data.jsx
+++ src/components/data/Data.jsx
@@ -3,7 +3,7 @@ import DataValues from "./values/DataValues";
 
 export default function Data() {
   return (
-    <section>
+    <section className="grid gap-4 bg-neu-very-pale-orange rounded-2xl p-4">
       <DataView />
       <DataValues />
     </section>
