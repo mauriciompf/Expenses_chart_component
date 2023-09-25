c3f386165b530e835fe48a18d2efea3068b35a5d
Maurício Farias <53566332+mauriciompf@users.noreply.github.com>
2023-09-25 19:23:26 -0400

    Update BalanceValue.jsx

--- src/components/balance/BalanceValue.jsx
+++ src/components/balance/BalanceValue.jsx
@@ -1,8 +1,8 @@
 export default function BalanceValue({ amount }) {
   return (
-    <p>
+    <p className="text-white">
       <span className="block">My balance</span>
-      <span className="block">${amount.toFixed(2)}</span>
+      <span className="block font-bold text-2xl">${amount.toFixed(2)}</span>
     </p>
   );
 }
