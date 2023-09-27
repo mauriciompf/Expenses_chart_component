b340774260225c8aec0d607582de6729c14d46ac
Maurício Farias <53566332+mauriciompf@users.noreply.github.com>
2023-09-26 20:14:48 -0400

    Update DataViewGraph.jsx

--- src/components/data/view/DataViewGraph.jsx
+++ src/components/data/view/DataViewGraph.jsx
@@ -1,21 +1,34 @@
+import React, { useState } from "react";
 import data from "../../../../data.json";
 
 export default function DataViewGraph() {
-  const list = [];
+  const currentDate = new Date();
+  const currentDay = currentDate.getDay() - 1;
 
-  const weekData = data.map((element, index) => ({
-    ...element,
-    id: index,
-  }));
+  const maxAmount = Math.max(...data.map((element) => element.amount));
 
-  weekData.forEach((element) => {
-    list.push(
-      <li className="grid items-center gap-2" key={element.id}>
+  const [hoveredAmount, setHoveredAmount] = useState(null);
+
+  const list = data.map((element, index) => {
+    const percentage = (element.amount / maxAmount) * 100;
+
+    const backgroundColorClass =
+      index === currentDay ? "bg-pri-cyan" : "bg-pri-soft-red";
+
+    return (
+      <li className="text-center relative" key={index}>
         <div
-          className="bg-pri-soft-red w-8 rounded-sm"
-          style={{ height: element.amount * 3 }}
+          className={`cursor-pointer w-8 rounded-sm ${backgroundColorClass} hover:opacity-50 mb-2`}
+          style={{ height: percentage }}
+          onMouseOver={() => setHoveredAmount(element.amount)}
+          onMouseOut={() => setHoveredAmount(null)}
         ></div>
-        <span className="text-center text-xs text-neu-medium-brown font-semibold">
+        {hoveredAmount === element.amount && (
+          <div className="text-xs bg-neu-park-brown text-white absolute w-auto p-1 rounded-md -left-[0.4rem] -top-7 font-bold">
+            ${hoveredAmount}
+          </div>
+        )}
+        <span className="block text-xs text-neu-medium-brown font-semibold">
           {element.day}
         </span>
       </li>
@@ -23,8 +36,10 @@ export default function DataViewGraph() {
   });
 
   return (
-    <ul className="flex justify-between items-end pb-4 border-b-4 border-b-neu-cream">
-      {list}
-    </ul>
+    <div>
+      <ul className="flex justify-between h-full items-end pb-4 border-b-4 border-b-neu-cream">
+        {list}
+      </ul>
+    </div>
   );
 }
